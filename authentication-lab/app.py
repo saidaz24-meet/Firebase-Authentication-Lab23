09c1b594d2e0f7aba8892106def3807b827d7838
saidaz24-meet <139681209+saidaz24-meet@users.noreply.github.com>
2023-07-23 23:06:47 +0300

    said is done with bonus

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -13,7 +13,7 @@ Config = {
   "databaseURL" : ""
     }
 
-firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(Config)
 auth = firebase.auth()
 
 
@@ -23,28 +23,41 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-      error = ""
-   if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
-            login_session['user'] = 
-auth.create_user_with_email_and_password(email, password)
-           return redirect(url_for('add_tweet'))
-       except:
-           error = "Authentication failed"
-   return render_template("signup.html")
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
+    return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
 
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
 
 if __name__ == '__main__':
     app.run(debug=True)
